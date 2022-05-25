57764f539677283170d3168e676f044c76e2046a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-05-25 15:49:15 -0400

    changed folders to uppercase names

--- Lesson3/app.ts
+++ Lesson3/app.ts
@@ -4,19 +4,19 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index'; 
+import indexRouter from './Routes/index'; 
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.use('/', indexRouter);
 
