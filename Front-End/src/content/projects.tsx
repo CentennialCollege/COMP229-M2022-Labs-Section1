8655738bd2f39b8f2ebf8165d6d1e4128c4a7c1e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-07-20 15:03:20 -0400

    added other content pages

---
+++ Front-End/src/content/projects.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+function Projects()
+{
+    useEffect(()=>{
+        document.title = "Our Projects";
+    });
+
+    return (
+        <div className="container">
+            <h1>Our Projects</h1>
+            <hr />
+            <p>Detail about Our Projects...</p> 
+        </div>
+    );
+}
+
+export default Projects;
---
+++ Front-End/src/content/services.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+function Services()
+{
+    useEffect(()=>{
+        document.title = "Our Services";
+    });
+
+    return (
+        <div className="container">
+            <h1>Our Services</h1>
+            <hr />
+            <p>Detail about Our Services...</p> 
+        </div>
+    );
+}
+
+export default Services;
