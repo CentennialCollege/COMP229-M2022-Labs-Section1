2e0b870a49e348b2c44766f5a658decf9799348e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-07-27 13:19:00 -0400

    completed auth-service

--- Front-End/src/services/auth-service.tsx
+++ Front-End/src/services/auth-service.tsx
@@ -1,6 +1,38 @@
+import http from '../components/http-common';
+
 class AuthService
 {
+    public login(username: string, password: string)
+    {
+        return http.post('/login', {username, password})
+        .then(response => {
+            if(response.data.token)
+            {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
+        })
+    }
 
+    public logout()
+    {
+        localStorage.removeItem("user");
+    }
+
+    public register(username: string, password: string, firstName: string, lastName: string, emailAddress: string)
+    {
+        return http.post('/register', {username, password, firstName, lastName, emailAddress});
+    }
+
+    getCurrentUser()
+    {
+        const userString = localStorage.getItem("user") as string;
+        if(userString)
+        {
+            return JSON.parse(userString);
+        }
+        return false;
+    }
 }
 
 export default new AuthService();
