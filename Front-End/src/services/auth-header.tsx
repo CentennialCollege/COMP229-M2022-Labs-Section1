bf79366c30bd2c0d88e30cc8f3cf7b949b838289
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-07-27 13:23:11 -0400

    completed auth-header service

--- Front-End/src/services/auth-header.tsx
+++ Front-End/src/services/auth-header.tsx
@@ -1,4 +1,16 @@
 export default function AuthHeader()
 {
-    
+    const userString = localStorage.getItem("user");
+    let user = null;
+
+    if(userString)
+    {
+        user = JSON.parse(userString);
+    }
+
+    if(user && user.token)
+    {
+        return { headers: {"Authorization": "Bearer" + user.token }};
+    }
+    return {};
 }
